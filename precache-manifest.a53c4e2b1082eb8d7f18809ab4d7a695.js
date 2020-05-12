415c10ccf85a5fd6f83789ee6930d0c09a4daf7e
Kiszuriwalilibori <kiszuriwalilibori@poczta.onet.pl>
2020-05-12 18:26:37 +0200

    Updates

---
+++ precache-manifest.a53c4e2b1082eb8d7f18809ab4d7a695.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3459f66f03057b4b499c7d66d9476784",
+    "url": "/crypto_redux_router/index.html"
+  },
+  {
+    "revision": "65a33bb58cce18cbbf56",
+    "url": "/crypto_redux_router/static/css/2.4181bbfd.chunk.css"
+  },
+  {
+    "revision": "65a33bb58cce18cbbf56",
+    "url": "/crypto_redux_router/static/js/2.94228e9e.chunk.js"
+  },
+  {
+    "revision": "ff72793476fa71facf46c608524577a9",
+    "url": "/crypto_redux_router/static/js/2.94228e9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb84da80add6768c9d75",
+    "url": "/crypto_redux_router/static/js/3.6987b863.chunk.js"
+  },
+  {
+    "revision": "e6a8002d361da96cbbff",
+    "url": "/crypto_redux_router/static/js/main.a7336a87.chunk.js"
+  },
+  {
+    "revision": "e8837c3b7daa5f431730",
+    "url": "/crypto_redux_router/static/js/runtime-main.0615a8cb.js"
+  },
+  {
+    "revision": "0424d5f5ed523a38a7f885c030f43c33",
+    "url": "/crypto_redux_router/static/media/background.0424d5f5.jpg"
+  }
+]);
